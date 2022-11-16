93d8b1db22c621c6e732c7a8843958d918087bd8
solovyovsdom <joelborofsky@mac.com>
2022-11-16 12:08:03 -0600

    completed main components

---
+++ src/components/Tabs/Tabs.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import {
+  BsFillHddStackFill,
+  BsPersonCircle,
+  BsLink,
+  BsBriefcaseFill,
+  BsEnvelope,
+} from 'react-icons/bs'
+import './Tabs.scss'
+
+export default function Tabs() {
+  const [activeTab, setActiveTab] = useState('stack')
+
+  return <div>Tabs</div>
+}
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,7 +1,19 @@
+import Tabs from '../../components/Tabs/Tabs'
 import './Main.scss'
+import Me from '../../../public/images/me1.jpg'
 
 export default function Main() {
   return (
-    <div className="Main">Main</div>
+    <header className='Main'>
+      <h2 className='Main__title'>Joel Borofsky</h2>
+      <img
+        src={Me}
+        alt='Joel Borofsky with a white cap on staring at the camera'
+        className='Main__img'
+      />
+      <h3 className="Main__subtitle">Frontend Developer</h3>
+      <p className="Main__tagline">A world of creative development</p>
+      <Tabs />
+    </header>
   )
 }
