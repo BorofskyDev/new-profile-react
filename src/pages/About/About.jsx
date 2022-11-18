21e98d0ddac1b3c83acd95c6497621c7a0f907f6
solovyovsdom <joelborofsky@mac.com>
2022-11-17 22:58:32 -0600

    updated social media links

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,6 +2,20 @@ import './About.scss'
 
 export default function About() {
   return (
-    <div>About</div>
+    <>
+      <h3 className='About'>About</h3>
+      <p className='About__content'>
+        {' '}
+        I am a frontend developer with an eye for design. I am unabashedly in
+        love with CSS, ThreeJS, and React Three Fiber (especially Drei!). But I
+        also love playing around with data and seeing how that data can
+        translate to the frontend, and have a firm belief that any design
+        project must begin and end with understanding the backend. In my
+        downtime you'll find me playing Cities: Skylines (a lot), working on my
+        own projects, taking walks, working out, and when time and money permit,
+        traveling. I enjoy experiencing different cultures and have a host of
+        places that I want to visit.
+      </p>
+    </>
   )
 }
--- src/pages/Links/Link.jsx
+++ src/pages/Links/Link.jsx
@@ -1,7 +1,50 @@
+import { FaGithub, FaLinkedinIn, FaTwitter, FaCodepen } from 'react-icons/fa'
 import './Link.scss'
 
 export default function Link() {
   return (
-    <div>Link</div>
+    <>
+      <div className='Link'>
+        <h3 className='Link__title'>Links</h3>
+        <div className='Link__container'>
+          <a
+            href='#'
+            style={{ '--clr': '#7313FF' }}
+            className='Link__links'
+            id='github'
+          >
+            {/* <span className='sr-only'>Github</span> */}
+            <FaGithub /> <span className='Link__links--name'>GitHub</span>
+          </a>
+          <a
+            href='#'
+            style={{ '--clr': '#00A4FF' }}
+            className='Link__links'
+            id='linkedin'
+          >
+            {/* <span className='sr-only'>LinkedIn</span> */}
+            <FaLinkedinIn /> <span className='Link__links--name'>LinkedIn</span>
+          </a>
+          <a
+            href='#'
+            style={{ '--clr': '#00B8FF' }}
+            className='Link__links'
+            id='twitter'
+          >
+            {/* <span className='sr-only'>Twitter</span> */}
+            <FaTwitter /> <span className='Link__links--name'>Twitter</span>
+          </a>
+          <a
+            href='#'
+            style={{ '--clr': '#F7F7F7' }}
+            className='Link__links'
+            id='codepen'
+          >
+            {/* <span className='sr-only'>CodePen</span> */}
+            <FaCodepen /> <span className='Link__links--name'>Codepen</span>
+          </a>
+        </div>
+      </div>
+    </>
   )
 }
